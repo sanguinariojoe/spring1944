90222ee090558b5108c89da8d56d59b400563993
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2017-12-18 17:44:32 +0100

    latest Code_Man spammer version

---
+++ LuaRules/Gadgets/s44_spam_assist.lua
@@ -0,0 +1,25 @@
+
+-- s44_spam_assist.lua
+
+--
+
+function gadget:GetInfo ()
+    return {
+        name      = "S44 Spam assist",
+        desc      = "Enables certain s44 specific things for spammer ai.",
+        author    = "Code_Man",
+        date      = "18/12/2017",
+        license   = "MIT X11",
+        layer     = 1,
+        enabled   = true
+    }
+end
+
+function gadget:Initialize ()
+    for _, t in ipairs (Spring.GetTeamList ()) do
+        local isAI = select (4, Spring.GetTeamInfo (t))
+        if isAI and Spring.GetTeamLuaAI (t) == "spammer" then
+            GG.S44_Spawn.SetSpawnFunc(t, function(t) return end)
+        end
+    end
+end
--- LuaRules/Gadgets/spam/main.lua
+++ LuaRules/Gadgets/spam/main.lua
@@ -5,11 +5,11 @@
 
 config = include ("LuaRules/Configs/spam_config.lua")
 
-local ai_list = {}
-local lifetime = {}
+local lifetime, ai_list = {}, {}
 local randomness_factor = 16 -- Must be an integer and larger than zero
+local multiplier = 1.0
 
-local function get_ai_instance (team)
+local function GetInstance (team)
     for _, i in ipairs (ai_list) do
         if i.team == team then
             return i
@@ -26,8 +26,8 @@ local function GetSpamTeams ()
     return l
 end
 
-local function GetSpamHq (team)
-    local instance = get_ai_instance (team)
+local function GetHq (team)
+    local instance = GetInstance (team)
     if instance ~= nil then
         return instance.hq
     end
@@ -35,10 +35,13 @@ local function GetSpamHq (team)
 end
 
 local function CalcUnitLongevity (unitDefID)
-    return 300 * Game.gameSpeed -- math.ceil ((Game.mapSizeX + Game.mapSizeZ) / UnitDefs[unitDefID].speed)
+    if UnitDefs[unitDefID].speed ~= 0 then
+        return math.ceil ((Game.mapSizeX + Game.mapSizeZ) / UnitDefs[unitDefID].speed * 100)
+    end
+    return nil
 end
 
-local function MakeSpamInstance (team)
+local function MakeInstance (team)
     local instance = {}
     instance.team = team
     instance.ally = 0
@@ -68,22 +71,15 @@ local function MakeSpamInstance (team)
     return instance
 end
 
-local function issue_build_order (instance, unitId, builder, x, y, z)
-    Spring.GiveOrderToUnit (builder, -unitId, {x, y, z}, {"shift"})
-    table.insert (instance.build_list, {unitId, builder})
-end
-
-local function build_order_completed (team, unit, unitDef)
-    --local build_list = get_ai_instance(unit).build_list
-    
-end
-
-local function apply_unit_bonus (unit)
-
+local function ApplyUnitBonus (unitID, hpBonus, losBonus)
+    Spring.SetUnitMaxHealth (unitID, select (2, Spring.GetUnitHealth (unitID)) * hpBonus) -- Don't set health because it starts at building
+    Spring.SetUnitSensorRadius (unitID, "los", losBonus * Spring.GetUnitSensorRadius (unitID, "los") * losBonus)
+    Spring.SetUnitSensorRadius (unitID, "radar", Spring.GetUnitSensorRadius (unitID, "radar") * losBonus)
+    Spring.SetUnitSensorRadius (unitID, "radarJammer", Spring.GetUnitSensorRadius (unitID, "radarJammer") * losBonus)
 end
 
 local function SetTarget (teamID, target)
-    local instance = get_ai_instance (teamID)
+    local instance = GetInstance (teamID)
     if instance == nil then
         return
     end
@@ -92,12 +88,17 @@ local function SetTarget (teamID, target)
         if target <= #Spring.GetTeamList () and not Spring.AreTeamsAllied (teamID, target) then
             x, y, z = Spring.GetTeamStartPosition (target)
             instance.current_enemy = target
+            local x, y, z = Spring.GetTeamStartPosition (instance.current_enemy)
+            for _, unit in ipairs (Spring.GetTeamUnits (instance.team)) do
+                Spring.GiveOrderToUnit (unit, CMD.FIGHT, {x, y, z}, {})
+                Spring.GiveOrderToUnit (unit, CMD.MOVE, {x, y, z}, {"shift"}) -- For units that cannot fight
+            end
         end
     elseif type (target) == "table" and #target == 3 then
         x = target[1]
-        y = target[2]
+        y = Spring.GetGroundHeight (x, z)
         z = target[3]
-        --instance.current_enemy = nil
+        instance.current_enemy = nil
     else
         target = nil
     end
@@ -107,7 +108,7 @@ local function SetTarget (teamID, target)
     end
 end
 
-local function make_random_build_order (build_list)
+local function MakeRandomBuildOrder (build_list)
     local new_list = {}
     for _, i in ipairs (build_list) do
         for j = 0, i[2] do
@@ -126,17 +127,17 @@ local function make_random_build_order (build_list)
 end
 
 local function StartSpam (team)
-    local instance = get_ai_instance (team)
+    local instance = GetInstance (team)
     if instance == nil then
         return nil
     end
-    for _, unit in ipairs (make_random_build_order (config.build_order)) do
+    for _, unit in ipairs (MakeRandomBuildOrder (config.build_order)) do
         Spring.GiveOrderToUnit (instance.hq, -(UnitDefNames[unit].id), {}, {"shift"})
     end
 end
 
 local function SpawnSpam (team)
-    local instance = get_ai_instance (team)
+    local instance = GetInstance (team)
     if instance == nil then
         return nil
     end
@@ -151,83 +152,83 @@ local function SpawnSpam (team)
             Spring.DestroyUnit (unit, false, true)
         end
     end
-    Spring.SetTeamRulesParam (team, "commander", "{{" .. "-1, " .. tostring (hq) .. "}}") -- Galactic conquest specific needed setting
-    if config.metal_income ~= nil then
-        Spring.SetUnitResourcing (hq, "umm", config.metal_income)
+    Spring.SetTeamRulesParam (team, "commander", "{{" .. "-1, " .. tostring (hq) .. "}}", {public = true}) -- Galactic conquest specific needed setting
+    if config.hq_metal_income ~= nil then
+        Spring.SetUnitResourcing (hq, "umm", config.metal_income * multiplier)
     end
-    if config.energy_income ~= nil then
-        Spring.SetUnitResourcing (hq, "ume", config.energy_income)
+    if config.hq_energy_income ~= nil then
+        Spring.SetUnitResourcing (hq, "ume", config.energy_income * multiplier)
+    end
+    if config.metal_storage then
+	Spring.SetTeamResource (team, "ms", config.metal_storage)
+	Spring.SetTeamResource (team, "m", config.metal_storage)
+    end
+    if config.energy_storage then
+	Spring.SetTeamResource (team, "es", config.energy_storage)
+	Spring.SetTeamResource (team, "e", config.energy_storage)
     end
     if config.hq_build_speed ~= nil then
-        Spring.SetUnitBuildSpeed (hq, config.hq_build_speed)
+        Spring.SetUnitBuildSpeed (hq, config.hq_build_speed * multiplier)
     end
     if config.hq_los ~= nil then
-        Spring.SetUnitSensorRadius (hq, "los", config.hq_los)
+        Spring.SetUnitSensorRadius (hq, "los", config.hq_los * multiplier)
     end
     if config.hq_hp ~= nil then
-        Spring.SetUnitMaxHealth (hq, config.hq_hp)
-        Spring.SetUnitHealth (hq, config.hq_hp)
+        Spring.SetUnitMaxHealth (hq, config.hq_hp * multiplier)
+        Spring.SetUnitHealth (hq, config.hq_hp * multiplier)
     end
-	-- Storage capacity
-	if config.hq_storage_metal then
-		Spring.SetTeamResource(team, "ms", config.hq_storage_metal)
-		Spring.SetTeamResource(team, "m", config.hq_storage_metal)
-	end
-	if config.hq_storage_energy then
-		Spring.SetTeamResource(team, "es", config.hq_storage_energy)
-		Spring.SetTeamResource(team, "e", config.hq_storage_energy)
-	end
     x, y, z = Spring.GetTeamStartPosition (instance.current_enemy)
     Spring.GiveOrderToUnit (hq, CMD.REPEAT, {1}, {}) -- Remove this when factories can idle in spring
-    Spring.GiveOrderToUnit (hq, CMD.FIGHT, {x, y, z}, {})
-    Spring.GiveOrderToUnit (hq, CMD.MOVE, {x, y, z}, {"shift"}) -- For units that cannot fight
-    GG.Delay.DelayCall (function (t) StartSpam (t) end, {team}, config.wait * Game.gameSpeed)
+    GG.Delay.DelayCall (function (t) StartSpam (t) end, {team}, config.wait * Game.gameSpeed * multiplier)
     return hq
 end
 
 function gadget:Initialize ()
     for _, t in ipairs (Spring.GetTeamList ()) do
-        local teamID, _, _, isAI = Spring.GetTeamInfo (t)
-        if isAI and Spring.GetTeamLuaAI (teamID) == gadget:GetInfo ().name then
-            table.insert (ai_list, MakeSpamInstance (teamID))
+        local isAI = select (4, Spring.GetTeamInfo (t))
+        if isAI and Spring.GetTeamLuaAI (t) == gadget:GetInfo ().name then
+            table.insert (ai_list, MakeInstance (t))
         end
     end
     if #ai_list == 0 then
         gadgetHandler:RemoveGadget ()
         return
     end
-    for index, unit in ipairs (config.build_order) do
+    local build_order = config.build_order
+    for index, unit in ipairs (build_order) do
         if UnitDefNames[unit[1]] == nil then
             Spring.Echo ("Invalid unitdef name " .. unit[1] .. " specified in spam build order")
             for i = index, #build_order do
-                config.build_order[i] = config.build_order[i + 1]
+                build_order[i] = build_order[i + 1]
             end
-            unit[#config.build_order] = nil
-        end    
+        end
     end
-    if #config.build_order == 0 then
+    if #build_order == 0 then
         for _, instance in ipairs (ai_list) do
             Spring.KillTeam (instance.team)
         end
-        Spring.Echo ("Empty spam build list, quitting please check LuaRules/Configs/spam_config.lua.")
+        Spring.Echo ("Empty spam build list, quitting check LuaRules/Configs/spam_config.lua.")
         gadgetHandler:RemoveGadget ()
         return
     end
+    config.build_order = build_order
     Spring.Echo ("SPAM loaded for: " .. Game.gameName)
     for _, instance in ipairs (ai_list) do
         GG.Delay.DelayCall (function (i) SpawnSpam (i) end, {instance.team}, 1) -- Because in the first frame units need to be spawned by default gadget
     end
     for _, p in ipairs (Spring.GetPlayerList ()) do
-        GG.Delay.DelayCall (Spring.SendMessageToPlayer, {p, config.warning}, config.wait * Game.gameSpeed)
+        GG.Delay.DelayCall (Spring.SendMessageToPlayer, {p, config.warning}, config.wait * Game.gameSpeed * multiplier)
     end
 end
 
 function gadget:UnitCreated (unitID, unitDefID, unitTeam, builderID)
-    local instance = get_ai_instance (unitTeam)
+    local instance = GetInstance (unitTeam)
     if instance ~= nil then
-        local maxHealth = select (2, Spring.GetUnitHealth (unitID))
-        Spring.GiveOrderToUnit (unitID, CMD.FIRE_STATE, {2}, {})
-        Spring.GiveOrderToUnit (unitID, CMD.MOVE_STATE, {2}, {})
+        x, y, z = Spring.GetTeamStartPosition (instance.current_enemy)
+        Spring.GiveOrderToUnit (unitID, CMD.FIRE_STATE, {2}, {"shift"})
+        Spring.GiveOrderToUnit (unitID, CMD.MOVE_STATE, {2}, {"shift"})
+        Spring.GiveOrderToUnit (unitID, CMD.FIGHT, {x, y, z}, {"shift"})
+        Spring.GiveOrderToUnit (unitID, CMD.MOVE, {x, y, z}, {"shift"}) -- For units that cannot fight
         --Spring.SetUnitMaxHealth (unitID, maxHealth + instance.health_coeff)
         if instance.unitCount + 1 == max_units and instance.unitCount < max_units then -- Units may be created by gadgets thus check for extra
             Spring.GiveOrderToUnit (instance.hq, CMD.WAIT, {}, {})
@@ -235,20 +236,27 @@ function gadget:UnitCreated (unitID, unitDefID, unitTeam, builderID)
                 Spring.SendMessageToPlayer (player, "Go forth my minions!")
             end
         end
-        instance.unitCount = instance.unitCount + 1
         if unitID ~= instance.hq and instance.hq ~= 0 then --
-            GG.Delay.DelayCall (Spring.DestroyUnit, {unitID, false}, CalcUnitLongevity (unitDefID))
+            instance.unitCount = instance.unitCount + 1
+            ApplyUnitBonus (unitID, config.unit_hp_multiplier * multiplier, config.unit_los_multiplier * multiplier)
         end
     end
 end
 
-function gadget:UnitFinished (unitID, unitDefID, team)
+function gadget:UnitFinished (unitID, unitDefID, unitTeam)
+    local instance = GetInstance (unitTeam)
+    if instance ~= nil then
+        if unitID ~= instance.hq and instance.hq ~= 0 then --
+            local l = CalcUnitLongevity (unitDefID)
+            GG.Delay.DelayCall (Spring.DestroyUnit, {unitID, false}, l)
+        end
+    end
 end
 
 function gadget:UnitFromFactory (unitID, unitDefID, unitTeam, factID, factDefID, userOrders)
-    local instance = get_ai_instance (unitTeam)
+    local instance = GetInstance (unitTeam)
     if instance ~= nil then
-        --Spring.GiveOrderToUnit (unitID, CMD.WAIT, {}, {}) -- This callin is broken in 103 it blocks factory
+        --Spring.GiveOrderToUnit (unitID, CMD.WAIT, {}, {}) -- This callin is broken in 104 it blocks factory
         table.insert (instance.current_squad, unitID)
         instance.current_squad_size = instance.current_squad_size + UnitDefs[unitDefID].metalCost
         if instance.current_squad_size >= 5000 then
@@ -261,8 +269,21 @@ function gadget:UnitFromFactory (unitID, unitDefID, unitTeam, factID, factDefID,
     end
 end
 
-function gadget:UnitDestroyed (unitID, unitDefID, unitTeam, attackerID, attackerDefID, attackerTeam) 
-    local instance = get_ai_instance (unitTeam)
+function gadget:UnitDamaged (unitID, unitDefID, unitTeam, damage, paralyzer, weaponDefID, projectileID, attackerID, attackerDefID, attackerTeam) 
+    local instance = GetInstance (unitTeam)
+    if instance ~= nil then
+        if unitID == instance.hq and attackerTeam ~= nil and attackerTeam >= 0 and not Spring.AreTeamsAllied (unitTeam, attackerTeam) and attackerTeam ~= Spring.GetGaiaTeamID () then
+            local x, y, z = Spring.GetTeamStartPosition (unitTeam)
+            local units = Spring.GetUnitsInSphere (x, y, z, 256, unitTeam)
+            --local x2, y2, z2 = Spring.GetTeamStartPosition (instance.current_enemy)
+            Spring.GiveOrderToUnitArray (units, CMD.INSERT, {0, CMD.ATTACK, 0, attackerID}, {"alt"})
+            --Spring.GiveOrderToUnitArray (units, CMD.INSERT, {0, CMD.FIGHT, CMD.OPT_SHIFT, x2, y2, z2}, {"alt"})
+        end
+    end
+end
+
+function gadget:UnitDestroyed (unitID, unitDefID, unitTeam, attackerID, attackerDefID, attackerTeam)
+    local instance = GetInstance (unitTeam)
     if instance ~= nil then
         if unitID == instance.hq then
             for _, p in ipairs (Spring.GetPlayerList ()) do
@@ -287,7 +308,7 @@ end
 
 function gadget:UnitEnteredLos (unitID, unitTeam, allyTeam, unitDefID)
     for _, team in pairs (Spring.GetTeamList (allyTeam)) do
-        local instance = get_ai_instance (team)
+        local instance = GetInstance (team)
         if instance ~= nil then
            if unitTeam == instance.current_enemy then
                local x, y, z = Spring.GetUnitPosition (unitID)
@@ -297,8 +318,9 @@ function gadget:UnitEnteredLos (unitID, unitTeam, allyTeam, unitDefID)
 end
 
 function gadget:UnitIdle (unitID, unitDefID, unitTeam)
-    if get_ai_instance (unitTeam) ~= nil then
-        if unitID == get_ai_instance (unitTeam).hq then
+    local instance = GetInstance (unitTeam)
+    if instance ~= nil then
+        if unitID == instance.hq then
             local maxHealth = select (2, Spring.GetUnitHealth (unitID))
             local metal, _, energy, _ = Spring.GetUnitResources (unitID)
             Spring.SetUnitResourcing (unitID, "umm", config.metal * config.hq_bonus_multiplier)
@@ -308,9 +330,9 @@ function gadget:UnitIdle (unitID, unitDefID, unitTeam)
             Spring.SetUnitSensorRadius (unitID, "los", Spring.GetUnitSensorRadius (unitID, "los") * config.hq_bonus_multiplier)
             Spring.SetUnitSensorRadius (unitID, "radar", Spring.GetUnitSensorRadius (unitID, "radar") * config.hq_bonus_multiplier)
             Spring.SetUnitSensorRadius (unitID, "radarJammer", Spring.GetUnitSensorRadius (unitID, "radarJammer") * config.hq_bonus_multiplier)
-            --for i = 1, 4 do
-                Spring.SetUnitWeaponState (unitID, 1, "range", Spring.GetUnitMaxRange (unitID) * config.hq_bonus_multiplier)
-            --end
+            for i = 1, 4 do
+                Spring.SetUnitWeaponState (unitID, i, "range", Spring.GetUnitMaxRange (unitID) * config.hq_bonus_multiplier)
+            end
             Spring.SetUnitMaxRange (unitID, Spring.GetUnitMaxRange (unitID) * config.hq_bonus_multiplier)
             for _, unit in ipairs (make_random_build_order (build_order)) do
                 GG.Delay.DelayCall (Spring.GiveOrderToUnit, {unitID, -(UnitDefNames[unit].id), {}, {"shift"}}, 1)
@@ -342,19 +364,14 @@ function gadget:TeamDied (team)
         end
         if #instance.enemies == 0 then -- Let the bots kill one another after game is over :p
             Spring.SetAlly (instance.team, team, false)
-            instance.current_enemy = instance.team
-        else
-            instance.current_enemy = instance.enemies[math.random (#instance.enemies)]
+            instance.enemies[1] = instance.team
         end
-        local x, y, z = Spring.GetTeamStartPosition (instance.current_enemy)
-        for _, unit in ipairs (Spring.GetTeamUnits (instance.team)) do
-            Spring.GiveOrderToUnit (unit, CMD.FIGHT, {x, y, z}, {})
-        end
+        SetTarget (instance.team, instance.enemies[math.random (#instance.enemies)])
     end
 end
 
-GG.Spam = {GetSpamTeams = GetSpamTeams,
-           GetSpamHq = GetSpamHq,
-           MakeSpamInstance = MakeSpamInstance,
-           SpawnSpam = SpawnSpam,
+GG.Spam = {GetTeams = GetSpamTeams,
+           GetHq = GetHq,
+           MakeInstance = MakeInstance,
+           Spawn = SpawnSpam,
            SetTarget = SetTarget}
