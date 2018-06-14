92998c1d51f609fd4da711982e2f56eb72ef2ad1
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2018-06-14 10:37:06 +0200

    First partially working custom map shader

--- LuaUI/Widgets/gfx_customMapShader.lua
+++ LuaUI/Widgets/gfx_customMapShader.lua
@@ -10,21 +10,22 @@ function widget:GetInfo()
    }
 end
 
-local glCreateShader = gl.CreateShader
-local glGetShaderLog = gl.GetShaderLog
+local glCreateShader       = gl.CreateShader
+local glGetShaderLog       = gl.GetShaderLog
 local glGetUniformLocation = gl.GetUniformLocation
-local glUseShader = gl.UseShader
-local glGetMatrixData = gl.GetMatrixData
-local glUniform = gl.Uniform
-local glUniformMatrix = gl.UniformMatrix
-local glGetSun = gl.GetSun
-local glGetAtmosphere = gl.GetAtmosphere
-local glGetMapRendering = gl.GetMapRendering      -- >= 104.0
-local glGetWaterRendering = gl.GetWaterRendering  -- >= 104.0
-local SetMapShader = Spring.SetMapShader
+local glUseShader          = gl.UseShader
+local glGetMatrixData      = gl.GetMatrixData
+local glUniform            = gl.Uniform
+local glUniformMatrix      = gl.UniformMatrix
+local glTexture            = gl.Texture
+local glGetSun             = gl.GetSun
+local glGetAtmosphere      = gl.GetAtmosphere
+local glGetMapRendering    = gl.GetMapRendering      -- >= 104.0
+local glGetWaterRendering  = gl.GetWaterRendering  -- >= 104.0
+local SetMapShader      = Spring.SetMapShader
 local GetCameraPosition = Spring.GetCameraPosition
-local HaveShadows = Spring.HaveShadows
-local GetMapDrawMode = Spring.GetMapDrawMode
+local HaveShadows       = Spring.HaveShadows
+local GetMapDrawMode    = Spring.GetMapDrawMode
 
 local shader
 local cameraPosID, lightDirID
@@ -42,6 +43,22 @@ local MAP_WIDTHPO2 = math.ceil(math.log(MAP_WIDTH / GRID_SIZE)) ^ 2 * GRID_SIZE
 local MAP_HEIGHTPO2 = math.ceil(math.log(MAP_HEIGHT / GRID_SIZE)) ^ 2 * GRID_SIZE
 
 
+local function readMapInfo()
+    local paths = {
+        "mapinfo.lua",
+        "maphelper/mapinfo.lua",
+    }
+    for _, v in ipairs(paths) do
+        if VFS.FileExists(v) then
+            return VFS.Include(v)
+        end
+    end
+
+    return nil
+end
+local MAP_INFO = nil -- readMapInfo()  -- Quite unsafe
+
+
 local function CompileShader()
     -- Original shaders
     local vertex = VFS.LoadFile("LuaUI\\Widgets\\Shaders\\GL3.X\\MapShaderVert.glsl", VFS.ZIP)
@@ -49,17 +66,19 @@ local function CompileShader()
     -- Shader definitions
     local definitions = {
         "#version 130",
-        "#define SMF_SPECULAR_LIGHTING",
-        "#define SMF_DETAIL_TEXTURE_SPLATTING",
-        "#define SMF_DETAIL_NORMAL_TEXTURE_SPLATTING",
-        "#define SMF_DETAIL_NORMAL_DIFFUSE_ALPHA",
-        "#define SMF_WATER_ABSORPTION",
-        "#define SMF_SKY_REFLECTIONS",
+        -- "#define SMF_SPECULAR_LIGHTING",
+        -- "#define SMF_DETAIL_TEXTURE_SPLATTING",
+        -- "#define SMF_DETAIL_NORMAL_TEXTURE_SPLATTING",
+        -- "#define SMF_DETAIL_NORMAL_DIFFUSE_ALPHA",
+        -- "#define SMF_WATER_ABSORPTION",
+        -- "#define SMF_SKY_REFLECTIONS",
         "#define SMF_BLEND_NORMALS",
-        "#define SMF_LIGHT_EMISSION",
-        "#define SMF_PARALLAX_MAPPING",
+        -- "#define SMF_LIGHT_EMISSION",
         "#define HAVE_INFOTEX",  -- We define it, and modify infoTexIntensityMul accordingly
     }
+    if MAP_INFO and MAP_INFO.resources and MAP_INFO.resources.parallaxheighttex then
+        definitions[#definitions + 1] = "#define SMF_PARALLAX_MAPPING"
+    end
     if glGetMapRendering and glGetMapRendering("voidWater") then
         definitions[#definitions + 1] = "#define SMF_VOID_WATER"
     end
@@ -67,16 +86,11 @@ local function CompileShader()
         definitions[#definitions + 1] = "#define SMF_VOID_GROUND"
     end
     if HaveShadows() then
-        definitions[#definitions + 1] = "#define HAVE_SHADOWS"
+        -- definitions[#definitions + 1] = "#define HAVE_SHADOWS"
     end
     definitions = table.concat(definitions, "\n")
     fragment = definitions .. "\n" .. fragment
 
-    Spring.Echo('-------------------------------------------------------------')
-    Spring.Echo(vertex)
-    Spring.Echo('-------------------------------------------------------------')
-    Spring.Echo(fragment)
-    Spring.Echo('-------------------------------------------------------------')
     -- Create the shader
     local shader = glCreateShader({
         vertex = vertex,
@@ -183,10 +197,25 @@ function widget:DrawWorld()
     else
         -- We use the default colors...
         x, y, z = 0.1, 0.1, 0.3
+        if MAP_INFO and MAP_INFO.water and MAP_INFO.water.mincolor then
+            x = MAP_INFO.water.mincolor[1]
+            y = MAP_INFO.water.mincolor[2]
+            z = MAP_INFO.water.mincolor[3]
+        end
         glUniform(waterMinColorID, x, y, z)
         x, y, z = 0.4, 0.6, 0.8
+        if MAP_INFO and MAP_INFO.water and MAP_INFO.water.basecolor then
+            x = MAP_INFO.water.basecolor[1]
+            y = MAP_INFO.water.basecolor[2]
+            z = MAP_INFO.water.basecolor[3]
+        end
         glUniform(waterBaseColorID, x, y, z)
         x, y, z = 0.004, 0.004, 0.002
+        if MAP_INFO and MAP_INFO.water and MAP_INFO.water.absorb then
+            x = MAP_INFO.water.absorb[1]
+            y = MAP_INFO.water.absorb[2]
+            z = MAP_INFO.water.absorb[3]
+        end
         glUniform(waterAbsorbColorID, x, y, z)
     end
     if glGetMapRendering then
@@ -196,17 +225,71 @@ function widget:DrawWorld()
         glUniform(splatTexMultsID, x, y, z, w)
     else
         x, y, z, w = 0.02, 0.02, 0.02, 0.02
+        if MAP_INFO and MAP_INFO.splats and MAP_INFO.splats.texscales then
+            x = MAP_INFO.splats.texscales[1]
+            y = MAP_INFO.splats.texscales[2]
+            z = MAP_INFO.splats.texscales[3]
+            z = MAP_INFO.splats.texscales[4]
+        end
         glUniform(splatTexScalesID, x, y, z, w)
         x, y, z, w = 1.0, 1.0, 1.0, 1.0
+        if MAP_INFO and MAP_INFO.splats and MAP_INFO.splats.texmults then
+            x = MAP_INFO.splats.texmults[1]
+            y = MAP_INFO.splats.texmults[2]
+            z = MAP_INFO.splats.texmults[3]
+            z = MAP_INFO.splats.texmults[4]
+        end
         glUniform(splatTexMultsID, x, y, z, w)
     end
 
     if GetMapDrawMode() == "normal" then
-        glUniform(infoTexIntensityMulID, 0.0)
+        glUniform(infoTexIntensityMulID, 1.0)
     else
         glUniform(infoTexIntensityMulID, 1.0)
     end
     -- Spring.GetMapDrawMode
 
     glUseShader(0)
+end
+
+local function SetTextures()
+    glTexture(0, "$shading")
+    glTexture(2, "$detail")
+    glTexture(4, "$shadow")
+    glTexture(5, "$normals")
+    glTexture(6, "$ssmf_specular")
+    glTexture(7, "$ssmf_splat_detail")
+    glTexture(8, "$ssmf_splat_distr")
+    glTexture(9, "$map_reflection")
+    glTexture(10, "$sky_reflection")
+    glTexture(11, "$ssmf_normals")
+    glTexture(12, "$ssmf_emission")
+    if MAP_INFO and MAP_INFO.resources.parallaxheighttex then
+        glTexture(13, "$ssmf_parallax")
+    end
+    glTexture(14, "$info")
+    glTexture(15, "$ssmf_splat_normals:0")
+    glTexture(16, "$ssmf_splat_normals:1")
+    glTexture(17, "$ssmf_splat_normals:2")
+    glTexture(18, "$ssmf_splat_normals:3")
+end
+
+function widget:DrawGroundPreForward()
+    SetTextures()
+end
+
+function widget:DrawGroundPreDeferred()
+    SetTextures()
+end
+
+local function UnsetTextures()
+    glTexture(0, false)
+    glTexture(2, false)
+    for i=4,18 do
+        glTexture(i, false)
+    end
+end
+
+function widget:DrawGroundPostDeferred()
+    UnsetTextures()
 end
