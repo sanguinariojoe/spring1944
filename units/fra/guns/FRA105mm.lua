b539c415c39767ad64f9dc98cd30dc45baaed6c5
Jose Luis Cercos-Pita <jlcercos@gmail.com>
2018-09-18 16:31:43 +0200

    FRA105mmMle1935B-truck model (soldier pending)

--- units/fra/guns/FRA105mm.lua
+++ units/fra/guns/FRA105mm.lua
@@ -5,7 +5,7 @@ local FRA_105mm_Truck = HGunTractor:New{
 	trackWidth				= 12,
 	objectName              = "FRA/FRA105mmMle1935B_truck.s3o",
 	customParams = {
-
+		normaltex			= "unittextures/FRACitroenP17_normals.dds",
 	},
 }
 
